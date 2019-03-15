2b9417c66ff0ecede125a21efda84a06c2ac3118
SuHongYueshy <346250026@qq.com>
2019-03-15 17:23:11 +0800

    new

---
+++ src/main/java/demo/BigDecimalTest.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+    public static void main(String[] args) {
+        double x = 1.0;
+        double y =  0.99;
+
+        double z = x - y;
+
+        System.out.println(z);
+
+        BigDecimal a = new BigDecimal(String.valueOf(x));
+        BigDecimal b = new BigDecimal(String.valueOf(y));
+
+        z = a.subtract(b).doubleValue();    // 进行减法运算时，加doubleValue（）可以避免损失精度
+
+        System.out.println(z);
+    }
+}
